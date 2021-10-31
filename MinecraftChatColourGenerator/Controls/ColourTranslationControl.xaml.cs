7029001fbb3a10e494d1422a5a8fb9450598136a
reghzy <kettlesimzulator420@gmail.com>
2021-10-31 14:20:16 +0000

    Add project files.

---
+++ MinecraftChatColourGenerator/Controls/ColourTranslationControl.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MinecraftChatColourGenerator.Controls {
+    /// <summary>
+    /// Interaction logic for ColourTranslationControl.xaml
+    /// </summary>
+    public partial class ColourTranslationControl : UserControl {
+        public DependencyProperty ColourProperty =
+            DependencyProperty.Register(
+                "Colour", 
+                typeof(Color), 
+                typeof(ColourTranslationControl), 
+                new FrameworkPropertyMetadata(Colors.White, FrameworkPropertyMetadataOptions.AffectsRender, (o, e) => {
+
+                }));
+
+        public ColourTranslationControl() {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e) {
+
+        }
+    }
+}
---
+++ MinecraftChatColourGenerator/TranslationMapWindow.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace MinecraftChatColourGenerator {
+    /// <summary>
+    /// Interaction logic for TranslationMapWindow.xaml
+    /// </summary>
+    public partial class TranslationMapWindow : Window {
+        public TranslationMapWindow() {
+            InitializeComponent();
+        }
+    }
+}
